c2ffc4118860397512c66c20faf084fbb55bf134
Michael Morais <michael_morais@Outlook.com.br>
2024-02-09 07:32:41 -0300

    exames section done

---
+++ components/exames/index.js
@@ -0,0 +1,20 @@
+import { Container } from "./style"
+export default function Exames() {
+    return (
+        <Container>
+            <h1>Pedidos de Exames</h1>
+
+
+            <p>No processo de avaliação nutricional, uma etapa muito importante é a avaliação bioquímica.
+                No entanto, nada em excesso será pedido, sobretudo se algum check up recente já tiver sido feito.</p>
+
+            <p>O excesso de exames não quer dizer que o paciente estará mais seguro. Na verdade,
+                eles podem até mesmo enviesar algum diagnóstico.</p>
+
+            <p>A fim de evitar gastos desnecessários e fazer uma bateria de exames
+                desnecessários só porque estão na moda, as recomendações de pedidos sempre terão um motivo investigativo.</p>
+
+            <p>O objetivo é garantir que cada solicitação tenha um propósito claro, respaldados em um porquê.</p>
+        </Container>
+    )
+}
